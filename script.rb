78c2c0ad9d6e579811650ddfac6ea02bf2ee8aba
Nick Yaremovich <nickyaremovich@gmail.com>
2026-05-18 17:30:45 -0600

    implement substring method

---
+++ script.rb
@@ -0,0 +1,13 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(strings, dictionary)
+  result = {} #start new hash
+  lowercase = strings.downcase #edge case for case
+  dictionary.each do |word|
+    count = lowercase.scan(word).count
+    result[word] = count if count > 0
+  end
+  result 
+end
+
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
